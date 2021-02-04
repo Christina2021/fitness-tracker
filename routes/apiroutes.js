4b3089eeb8e0e230834c161129acf4fe651e5e6e
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-02-03 21:29:38 -0800

    Add put request to add an exercise to a workout.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -32,7 +32,16 @@ apiRouter.post("/api/workouts", (req, res) => {
         });
 });
 
-// put /api/workouts/:id
+// Add exercise to workout
+apiRouter.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate({_id: ObjectId(req.params.id)}, { $push: { exercises: req.body } }, { new: true })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 // get /api/workouts/range
 
